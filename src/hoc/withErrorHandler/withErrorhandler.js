9e47993004cfa604e2d288247814345f971031b6
Salvador <Salvador@cocard.info>
2018-09-05 11:48:36 -0400

    Added Ingredients to the server and you now pull from server for all the ingredients

--- src/hoc/withErrorHandler/withErrorhandler.js
+++ src/hoc/withErrorHandler/withErrorhandler.js
@@ -6,17 +6,24 @@ import Aux from '../Auxjs';
 const withErrorHandler = (WrappedComonenet, axios) => {
     return class extends Component {
         state = {
-            error: null
+            error: null,
+            reqInterceptor: null
         }
 
-        componentDidMount() {
-            axios.interceptors.request.use(req => {
+        componentWillMount() {
+            this.reqInterceptor = axios.interceptors.request.use(req => {
                 this.setState({error: null});
                 return req;
             });
-            axios.interceptors.response.use(res => res, error => {
+            this.reqInterceptor = axios.interceptors.response.use(res => res, error => {
                 this.setState({error: error});
             });
+        }
+
+
+        componentWillUnmount() {
+            axios.interceptors.request.eject(this.reqInterceptor);
+            axios.interceptors.response.eject(this.reqInterceptor);
         }
 
         errorConfirmedHandler = () => {
